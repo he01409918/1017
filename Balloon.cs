c2c33dad3a643b984d9f4b1582b4a5e00099788f
he01409918 <92556503+he01409918@users.noreply.github.com>
2022-10-24 11:42:40 +0800

    Add files via upload

---
+++ Balloon.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balloon : MonoBehaviour
+{
+    public int score;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Bullet"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
