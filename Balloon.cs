446cf8d74b3561291a89ecdb55bd78af13f723e4
he01409918 <92556503+he01409918@users.noreply.github.com>
2022-10-31 09:36:18 +0800

    Update Balloon.cs

--- Balloon.cs
+++ Balloon.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -9,6 +9,7 @@ public class Balloon : MonoBehaviour
     {
         if (collision.collider.CompareTag("Bullet"))
         {
+            Manager.Instance.OnAddScore(score);
             Destroy(gameObject);
         }
     }
