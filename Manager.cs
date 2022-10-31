b2f78ccc5bbc4b485d350dd7c83d6a9a29be7a67
he01409918 <92556503+he01409918@users.noreply.github.com>
2022-10-31 11:33:56 +0800

    Update Manager.cs

--- Manager.cs
+++ Manager.cs
@@ -22,6 +22,8 @@ public class Manager : MonoBehaviour
     {
         Vector3 position = new Vector3(Random.Range(3f,18f), Random.Range(0f,3f), Random.Range(23f,33f));
         GameObject _balloon = Instantiate(balloon[Random.Range(0, balloon.Length)], position, this.transform.rotation);
+        Destroy(_balloon, Random.Range(5f, 10f));
+        Invoke(nameof(InitBalloon), Random.Range(0.5f, 2));
     }
 
     public void OnAddScore(int value)
