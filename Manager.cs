e97b75e889151355b868a9aa686c18f1218df2f8
he01409918 <92556503+he01409918@users.noreply.github.com>
2022-10-31 09:54:00 +0800

    Update Manager.cs

--- Manager.cs
+++ Manager.cs
@@ -1,17 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Manager : MonoBehaviour
 {
     public static Manager Instance;
     public int currentScore;
+
+    public Text scoreText;
+
     void Start()
     {
         Instance = this;
+        OnAddScore(0);
     }
     public void OnAddScore(int value)
     {
         currentScore += value;
+        scoreText.text = currentScore.ToString();
     }
 }
